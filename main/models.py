6e350e49d03a856b8e4762892adbb8b641dc6748
Benson Chiang <benson0605@gmail.com>
2023-09-07 15:03:37 +0800

    Site Pagination and Blog Ordering
    Finished pagination feature and ordered blogs on the home page showing most recent posts first

--- main/models.py
+++ main/models.py
@@ -14,6 +14,10 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
